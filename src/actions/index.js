1bc15d472a2078a906c2dffbfcd4dc50895d7a49
rojrun <runroj@gmail.com>
2019-01-23 16:15:04 -0800

    chat completed

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,20 @@
 import types from './types';
+import { db } from '../firebase';
+
+export const getMessages = () => dispatch => {
+    const dbRef = db.ref('/chat-log');
+
+    dbRef.on('value', (snapshot) => {
+        console.log('The DB has changed!');
+        dispatch({
+            type: types.GET_CHAT_MESSAGES,
+            messages: snapshot.val()
+        });
+    });
+
+    return dbRef;
+}
+
+export const sendMessage = message => dispatch => {
+    db.ref('/chat-log').push(message);
+}
---
+++ src/config/firebase.js
@@ -0,0 +1,8 @@
+export default {
+    apiKey: "AIzaSyBRgU2u9XyQKu-NpNcAanYrwHGiRZIigLY",
+    authDomain: "chat-app-8c43b.firebaseapp.com",
+    databaseURL: "https://chat-app-8c43b.firebaseio.com",
+    projectId: "chat-app-8c43b",
+    storageBucket: "chat-app-8c43b.appspot.com",
+    messagingSenderId: "120684879753"
+};
